db3b3fc02a366707f81a2e83ab7ac36e72e451a6
COTTAGE\赵辉辉 <zhaohuihui@outlook.com>
2017-05-03 15:39:46 +0800

    00

---
+++ src/JK3DEx.java
@@ -0,0 +1,15 @@
+import de.onlinehome.geomath.jk3d.jk3d;
+
+public class JK3DEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String path="C:/Users/zhaohuihui/workspace/JK3DTest/jk3d.par";//第一个文件的路径
+		jk3d j=new jk3d(path);
+		
+		System.out.println("插值完成");
+	}
+	
+
+
+}
